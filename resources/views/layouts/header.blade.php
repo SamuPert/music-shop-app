7bbe01074132755d09902d6e874dec8c4eea0b91
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-04-19 16:43:05 +0200

    Aggiunte librerie per bootstrap.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,9 @@
-<section class="mt-3">
+<section>
     <div>
         <div class="row">
-            <div class="col col-md-2 col-sm-2 col-2"><img /></div>
+            <div class="col col-md-2 col-sm-2 col-2">
+                <img src="{{ asset("/img/logoSito.png") }}" class="image-fluid w-100 mt-2 ml-2"/>
+            </div>
             <div class="col col-md-10">
                 <section>
                     <div class="row">
@@ -9,41 +11,82 @@
                             <div class="row">
                                 <div class="col">
                                     <div class="input-group mt-2 mb-2">
-                                        <div class="input-group-prepend"><select><optgroup label="This is a group"><option value="12" selected>This is item 1</option><option value="13">This is item 2</option><option value="14">This is item 3</option></optgroup></select></div><input type="text"
-                                                                                                                                                                                                                                                                                           class="form-control" />
-                                        <div class="input-group-append"><button class="btn btn-primary" type="button">Cerca</button></div>
+                                        <div class="input-group-prepend"><select>
+                                                <optgroup label="This is a group">
+                                                    <option value="12" selected>This is item 1</option>
+                                                    <option value="13">This is item 2</option>
+                                                    <option value="14">This is item 3</option>
+                                                </optgroup>
+                                            </select></div>
+                                        <input type="text"
+                                               class="form-control"/>
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="button">Cerca</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col d-none d-md-block"><img src="{{asset("/img/logoSito.png")}}" /></div>
+                        <div class="col d-none d-md-block">
+                            Carrello ecc..
+                        </div>
                     </div>
                     <div class="row">
                         <div class="col">
-                            <nav class="navbar navbar-light navbar-expand-md float-right float-md-left">
-                                <div class="container-fluid"><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                            <div class="w-100">
+                                <button data-toggle="collapse" data-target="#navcol-1"
+                                    class="navbar navbar-toggler navbar-light navbar-collapse navbar-toggler-right float-right menu-toggle">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <nav class="navbar navbar-expand-md float-right float-md-left w-100">
+                                <div class="container-fluid ">
                                     <div class="collapse navbar-collapse" id="navcol-1">
-                                        <div class="row d-sm-block d-md-none">
-                                            <div class="input-group mt-2 mb-2">
-                                                <div class="input-group-prepend"><select><optgroup label="This is a group"><option value="12" selected>This is item 1</option><option value="13">This is item 2</option><option value="14">This is item 3</option></optgroup></select></div><input
-                                                    type="text" class="form-control" />
-                                                <div class="input-group-append"><button class="btn btn-primary" type="button">Cerca</button></div>
-                                            </div>
-                                        </div>
+
                                         <ul class="nav navbar-nav">
-                                            <li role="presentation" class="nav-item"></li>
-                                            <li role="presentation" class="nav-item"></li>
-                                            <li role="presentation" class="nav-item"></li>
-                                            <li class="nav-item dropdown"><a data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link" href="#">Dropdown </a>
-                                                <div role="menu" class="dropdown-menu"><a role="presentation" class="dropdown-item" href="#">First Item</a><a role="presentation" class="dropdown-item" href="#">Second Item</a><a role="presentation" class="dropdown-item" href="#">Third Item</a></div>
+                                            <li class="nav-item">
+                                                <div class="row d-sm-block d-md-none mr-0 ml-0 w-100">
+                                                    <div class="input-group mt-2 mb-2">
+                                                        <div class="input-group-prepend"><select>
+                                                                <optgroup label="This is a group">
+                                                                    <option value="12" selected>This is item 1</option>
+                                                                    <option value="13">This is item 2</option>
+                                                                    <option value="14">This is item 3</option>
+                                                                </optgroup>
+                                                            </select></div>
+                                                        <input
+                                                            type="text" class="form-control"/>
+                                                        <div class="input-group-append">
+                                                            <button class="btn btn-primary" type="button">Cerca</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </li>
-                                            <li class="nav-item dropdown"><a data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link" href="#">Dropdown </a>
-                                                <div role="menu" class="dropdown-menu"><a role="presentation" class="dropdown-item" href="#">First Item</a><a role="presentation" class="dropdown-item" href="#">Second Item</a><a role="presentation" class="dropdown-item" href="#">Third Item</a></div>
+                                            <li class="nav-item dropdown"><a data-toggle="dropdown"
+                                                                             aria-expanded="false"
+                                                                             class="dropdown-toggle nav-link" href="#">Dropdown </a>
+                                                <div role="menu" class="dropdown-menu"><a role="presentation"
+                                                                                          class="dropdown-item"
+                                                                                          href="#">First Item</a><a
+                                                        role="presentation" class="dropdown-item" href="#">Second
+                                                        Item</a><a role="presentation" class="dropdown-item" href="#">Third
+                                                        Item</a></div>
+                                            </li>
+                                            <li class="nav-item dropdown"><a data-toggle="dropdown"
+                                                                             aria-expanded="false"
+                                                                             class="dropdown-toggle nav-link" href="#">Dropdown </a>
+                                                <div role="menu" class="dropdown-menu"><a role="presentation"
+                                                                                          class="dropdown-item"
+                                                                                          href="#">First Item</a><a
+                                                        role="presentation" class="dropdown-item" href="#">Second
+                                                        Item</a><a role="presentation" class="dropdown-item" href="#">Third
+                                                        Item</a></div>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
                             </nav>
+                            </div>
                         </div>
                     </div>
                 </section>
