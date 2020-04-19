61201327d9e734f31d9f1f8e3e396cad9599721d
Claudio Sirocchi <45624527+ClaudioSirocchi@users.noreply.github.com>
2020-04-19 16:44:14 +0200

    Aggiunta Menu
    Aggiunto menu sia in formato standard che responsive

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,53 @@
-footer
+<section class="mt-3">
+    <div>
+        <div class="row">
+            <div class="col col-md-2 col-sm-2 col-2"><img /></div>
+            <div class="col col-md-10">
+                <section>
+                    <div class="row">
+                        <div class="col d-none d-md-block col-md-8 col-sm-12 ml-0 mr-0">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="input-group mt-2 mb-2">
+                                        <div class="input-group-prepend"><select><optgroup label="This is a group"><option value="12" selected>This is item 1</option><option value="13">This is item 2</option><option value="14">This is item 3</option></optgroup></select></div><input type="text"
+                                                                                                                                                                                                                                                                                           class="form-control" />
+                                        <div class="input-group-append"><button class="btn btn-primary" type="button">Cerca</button></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col d-none d-md-block"><img /></div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <nav class="navbar navbar-light navbar-expand-md float-right float-md-left">
+                                <div class="container-fluid"><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                                    <div class="collapse navbar-collapse" id="navcol-1">
+                                        <div class="row d-sm-block d-md-none">
+                                            <div class="input-group mt-2 mb-2">
+                                                <div class="input-group-prepend"><select><optgroup label="This is a group"><option value="12" selected>This is item 1</option><option value="13">This is item 2</option><option value="14">This is item 3</option></optgroup></select></div><input
+                                                    type="text" class="form-control" />
+                                                <div class="input-group-append"><button class="btn btn-primary" type="button">Cerca</button></div>
+                                            </div>
+                                        </div>
+                                        <ul class="nav navbar-nav">
+                                            <li role="presentation" class="nav-item"></li>
+                                            <li role="presentation" class="nav-item"></li>
+                                            <li role="presentation" class="nav-item"></li>
+                                            <li class="nav-item dropdown"><a data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link" href="#">Dropdown </a>
+                                                <div role="menu" class="dropdown-menu"><a role="presentation" class="dropdown-item" href="#">First Item</a><a role="presentation" class="dropdown-item" href="#">Second Item</a><a role="presentation" class="dropdown-item" href="#">Third Item</a></div>
+                                            </li>
+                                            <li class="nav-item dropdown"><a data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link" href="#">Dropdown </a>
+                                                <div role="menu" class="dropdown-menu"><a role="presentation" class="dropdown-item" href="#">First Item</a><a role="presentation" class="dropdown-item" href="#">Second Item</a><a role="presentation" class="dropdown-item" href="#">Third Item</a></div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</section>
