6ba2d5e3a7733b3bb77089f083ea0891c62dfa8c
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-04-20 16:37:33 +0200

    Aggiunto il footer. Da completare.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,38 @@
+<div class="footer-clean shadow">
+    <footer>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Web design</a></li>
+                        <li><a href="#">Development</a></li>
+                        <li><a href="#">Hosting</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Legacy</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Careers</h3>
+                    <ul>
+                        <li><a href="#">Job openings</a></li>
+                        <li><a href="#">Employee success</a></li>
+                        <li><a href="#">Benefits</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 item social">
+                    <a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
+                    <a href="mailto:info_FAKE@musicshop.it"><i class="fa fa-at"></i></a>
+                    <p class="copyright">Music Shop ©{{ date('Y') }}</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
