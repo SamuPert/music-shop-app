73da6808b2ff3c5ec61e8e2b7f98163d0e2f2b3b
Luca Munerati <51243369+MuneLuca98@users.noreply.github.com>
2020-05-11 17:13:57 +0200

    Aggiunto contenuto all'header
    Co-Authored-By: laurapistagnesi <laurapistagnesi@users.noreply.github.com>

--- resources/views/layouts/header_admin_staff.blade.php
+++ resources/views/layouts/header_admin_staff.blade.php
@@ -3,12 +3,18 @@
         <img onclick="document.location.href='/'" src="{{ asset("/img/logoSitoMobile.png") }}" class="show-on-desktop image-fluid h-100 mt-2 ml-2"  style="max-height: 120px; cursor: pointer;" />
         <img onclick="document.location.href='/'" src="{{ asset("/img/logoSitoMobile.png") }}" class="show-on-mobile image-fluid h-100 mt-2 ml-2"  style="max-height: 70px; cursor: pointer;" />
     </div>
-    <div class="col d-none d-md-block mt-2 mr-2" >
-        <div class="float-right mt-2 ml-1">
+    <div class="col-md-10 mt-5">
+    <div class="row">
+        <div class="col-md-9" style="text-align: center">
+            <h2 class="text-center">Funzionalità Admin </h2>
+            <p class="text-center">Hai eseguito il login da Utente Admin,queste sono le funzionalità a tua disposizione.</p>
+        </div>
+        <div class="float-right col-md-3">
             <a href="#panelLogout" class="btn btn-light float-right modal-toggle">
                 Logout
                 <i class="fa fa-sign-out" aria-hidden="true"></i>
             </a>
         </div>
+    </div>
     </div>
 </div>
