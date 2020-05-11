d1752b46c1569d368193634c65336b32da6d20b6
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-05-11 16:52:13 +0200

    Unite tutte le librerie js in app.js e quelle css in app.css

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,50 @@ $('.modal-toggle').click(function(e){
     var tab = e.target.hash;
     $('li > a[href="' + tab + '"]').tab("show");
 });
+
+window.toggleFiltriBar = () =>
+{
+    let toggleIcon = $('#iconToggleFiltri');
+    let toggleDiv = $('#filtriInnerBar');
+
+    if( toggleDiv.hasClass('show') )
+    {
+        toggleDiv.collapse('hide');
+        toggleIcon.removeClass('fa-chevron-up');
+        toggleIcon.addClass('fa-chevron-down');
+    }else{
+        toggleDiv.collapse('show');
+        toggleIcon.removeClass('fa-chevron-down');
+        toggleIcon.addClass('fa-chevron-up');
+    }
+}
+
+window.applyFilters = () =>
+{
+    alert(1);
+}
+
+
+function registerRangeInputChange(id_range_input, id_html_value_output)
+{
+    // Read value on page load
+    $(id_html_value_output).html($(id_range_input).val());
+
+    // Read value on change
+    $(id_range_input).on('input', function(){
+        let current_value = $(this).val() + '';
+
+
+        let tmpOutput = '';
+
+        for( let i = current_value.length; i < 7; i++ ){
+            tmpOutput += '&nbsp;';
+        }
+
+        $(id_html_value_output).html(tmpOutput + current_value);
+    });
+
+}
+
+registerRangeInputChange('#prezzoMaxInput', '#prezzoMaxValue');
+registerRangeInputChange('#prezzoMinInput', '#prezzoMinValue');
