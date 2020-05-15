9356d75c02d005b3d0efe84a46b39244020a92db
laurapistagnesi <49392902+laurapistagnesi@users.noreply.github.com>
2020-05-15 12:20:05 +0200

    Aggiunte immagini alle categorie e sottocategorie

--- database/seeds/CategoriaSeeder.php
+++ database/seeds/CategoriaSeeder.php
@@ -14,32 +14,32 @@ class CategoriaSeeder extends Seeder
         $categorie = [
             [
                 'nome_categoria'        => 'Chitarre',
-                'percorso_foto'         => '',
+                'percorso_foto'         => '/img/Chitarre.jpg',
                 'descrizione_categoria' => ''
             ],
             [
                 'nome_categoria'        => 'Bassi',
-                'percorso_foto'         => '',
+                'percorso_foto'         => '/img/Bassi.jpg',
                 'descrizione_categoria' => ''
             ],
             [
                 'nome_categoria'        => 'Tastiere',
-                'percorso_foto'         => '',
+                'percorso_foto'         => '/img/Tastiere.jpg',
                 'descrizione_categoria' => ''
             ],
             [
                 'nome_categoria'        => 'Batterie',
-                'percorso_foto'         => '',
+                'percorso_foto'         => '/img/Batterie.jpg',
                 'descrizione_categoria' => ''
             ],
             [
                 'nome_categoria'        => 'Fiati',
-                'percorso_foto'         => '',
+                'percorso_foto'         => '/img/Fiati.jpg',
                 'descrizione_categoria' => ''
             ],
             [
                 'nome_categoria'        => 'Attrezzature',
-                'percorso_foto'         => '',
+                'percorso_foto'         => '/img/Attrezzature.jpg',
                 'descrizione_categoria' => ''
             ],
         ];
--- database/seeds/SottocategoriaSeeder.php
+++ database/seeds/SottocategoriaSeeder.php
@@ -17,117 +17,117 @@ class SottocategoriaSeeder extends Seeder
                 [
                     'nome_sotto_categoria'        => 'Elettriche',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Elettrica.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Classiche',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Classica.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Acustiche',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Acustica.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Semiacustiche',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Semiacustica.jpg'
                 ],
             ],
             'Bassi' => [
                 [
                     'nome_sotto_categoria'        => 'Elettrici',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/BassoElettrico.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Acustici',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/BassoAcustico.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Contrabbassi',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/contrabbasso.JPG'
                 ],
             ],
             'Tastiere' => [
                 [
                     'nome_sotto_categoria'        => 'Workstation',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Tastiera.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Fisarmoniche',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Fisarmonica.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Pianoforti digitali',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Pianoforte.jpg'
                 ],
             ],
             'Batterie' => [
                 [
                     'nome_sotto_categoria'        => 'Acustiche',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/BatteriaAcustica.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Elettroniche',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/BatteriaElettronica.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Percussioni',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Percussioni.jpg'
                 ],
             ],
             'Fiati' => [
                 [
                     'nome_sotto_categoria'        => 'Sax',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => 'img/Sassofono.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Trombe',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Trombe.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Flauti',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Flauti.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Clarinetti',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Clarinetto.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Altri strumenti a fiato',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/AltriFiati.jpg'
                 ],
             ],
             'Attrezzature' => [
                 [
                     'nome_sotto_categoria'        => 'Amplificatori',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Amplificatori.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Cavi e connettori',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Connettori.jpg'
                 ],
                 [
                     'nome_sotto_categoria'        => 'Supporti',
                     'descrizione'                 => '',
-                    'percorso_foto'               => ''
+                    'percorso_foto'               => '/img/Supporti.jpg'
                 ],
             ]
         ];
