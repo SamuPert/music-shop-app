92195dd269c649f3ea593688265021a46eff2e8e
laurapistagnesi <49392902+laurapistagnesi@users.noreply.github.com>
2020-05-22 16:15:40 +0200

    Aggiunta la lista dei prodotti per ogni sottocategoria

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Categoria;
+use App\Prodotto;
 use App\Sottocategoria;
-use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
 
 class CategoriaController extends Controller
 {
@@ -13,7 +15,13 @@ class CategoriaController extends Controller
         // Lista sotto categorie per categoria
         $categoria = Categoria::findOrFail($id_categoria);
         $sotto_categorie = Sottocategoria::where('id_categoria', $id_categoria)->get();
-
-        return view('listaSottoCategorie', compact(['categoria', 'sotto_categorie']));
+        $prodotti = DB::table('prodotto')
+            ->join('sotto_categoria', function ($join) use ($id_categoria) {
+                $join->on('prodotto.id_sotto_categoria', '=', 'sotto_categoria.id_sotto_categoria')
+                    ->where('id_categoria', $id_categoria)
+                    ->select('prodotto.*');
+            })
+            ->paginate(5);
+        return view('listaSottoCategorie', compact(['categoria', 'sotto_categorie', 'prodotti']));
     }
 }
--- resources/views/listaSottoCategorie.blade.php
+++ resources/views/listaSottoCategorie.blade.php
@@ -38,13 +38,43 @@
                         </div>
                         @endforeach
                     </div>
-
                 </div>
-
-
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        I nostri prodotti
+                    </h5>
+                    {{ $prodotti->links() }}
+                    <div class="row">
+                        <div class="col-lg-8 mx-auto">
+                            <!-- List group-->
+                            @foreach($prodotti as $prodotto)
+                                <ul class="list-group shadow">
+                                    <!-- list group item-->
+                                    <li class="list-group-item">
+                                        <!-- Custom content-->
+                                        <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                                            <div class="media-body order-2 order-lg-1">
+                                                <h5 class="mt-0 font-weight-bold mb-2">{{$prodotto->nome_prodotto}}</h5>
+                                                <p class="font-italic text-muted mb-0 small">Ciao</p>
+                                                <div class="d-flex align-items-center justify-content-between mt-1">
+                                                    <h6 class="font-weight-bold my-2">{{$prodotto->prezzo}}€</h6>
+                                                </div>
+                                            </div>
+                                            <img src="{{$prodotto->percorso_foto != '' ? asset( $prodotto->percorso_foto ) : 'https://via.placeholder.com/300x200.png' }}" alt="Generic placeholder image"
+                                                 width="200" class="ml-lg-5 order-1 order-lg-2">
+                                            <a href="{{ route('visualizza_prodotto', ['id_prodotto' => $prodotto->id_prodotto]) }}" class="stretched-link"></a>
+                                        </div> <!-- End -->
+                                    </li> <!-- End -->
+                                </ul>
+                            @endforeach
+                        </div>
+                    </div>
+                    {{ $prodotti->links() }}
+                </div>
             </div>
         </div>
     </div>
 </div>
-
 @endsection
