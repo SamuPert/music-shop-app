f08353679af73fe03ff3d9dfdb9c5cecf5bee984
SamuelePerticarari <samuele.perticarari@gmail.com>
2020-05-25 15:12:22 +0200

    Aggiornato seeder per il Prodotto

--- database/seeds/ProdottoSeeder.php
+++ database/seeds/ProdottoSeeder.php
@@ -15,115 +15,351 @@ class ProdottoSeeder extends Seeder
         $dati = [
             'Chitarre' => [
                 'Elettriche' => [
-                    'BOX SDC 33K',
-                    'DARESTONE ELG Black',
-                    'EKO S300 Black'
+                    'BOX SDC 33K' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'DARESTONE ELG Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'EKO S300 Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ]
                 ],
                 'Classiche' => [
-                    'YAMAHA CX40',
-                    'EKO CS5 Natural',
-                    'VALENCIA VC104 Natural',
+                    'YAMAHA CX40' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'EKO CS5 Natural' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'VALENCIA VC104 Natural' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Acustiche' => [
-                    'EASTMAN ACDR1',
-                    'DARESTONE AG1 Natural',
-                    'FENDER CD60S Black',
+                    'EASTMAN ACDR1' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'DARESTONE AG1 Natural' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'FENDER CD60S Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Semiacustiche' => [
-                    'IBANEZ AS53 TF Tobacco Flat',
-                    'GRETSCH G2420 Streamliner LR Walnut',
-                    'FENDER Squier Affinity Starcaster MN Black',
+                    'IBANEZ AS53 TF Tobacco Flat' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'GRETSCH G2420 Streamliner LR Walnut' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'FENDER Squier Affinity Starcaster MN Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
             ],
             'Bassi' => [
                 'Elettrici' => [
-                    'EKO VJB200 Black',
-                    'JACKSON JS1X Concert Minion AH Bass Black',
-                    'YAMAHA TRBX305 White',
-                    'EKO MM305 Black',
+                    'EKO VJB200 Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'JACKSON JS1X Concert Minion AH Bass Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA TRBX305 White' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'EKO MM305 Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Acustici' => [
-                    'KALA U-BASS Wanderer Acoustic-Electric',
-                    'IBANEZ PCBE12 OPN',
-                    'TAKAMINE GB72CE Natural',
+                    'KALA U-BASS Wanderer Acoustic-Electric' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'IBANEZ PCBE12 OPN' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'TAKAMINE GB72CE Natural' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Contrabbassi' => [
-                    'DIAMOND HB6076I Maestro 3/4',
-                    'VOX MEISTER DBASS34',
-                    'YAMAHA SLB200 Silent Bass',
+                    'DIAMOND HB6076I Maestro 3/4' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'VOX MEISTER DBASS34' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA SLB200 Silent Bass' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
             ],
             'Tastiere' => [
                 'Workstation' => [
-                    'KORG Krome EX 61',
-                    'ROLAND FA-06',
-                    'YAMAHA MONTAGE 7',
+                    'KORG Krome EX 61' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'ROLAND FA-06' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA MONTAGE 7' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Fisarmoniche' => [
-                    'SOUNDSATION Voce I 2648-RD',
-                    'HOHNER Bravo III 72 Black',
-                    'ROLAND FR-1XB Black',
+                    'SOUNDSATION Voce I 2648-RD' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'HOHNER Bravo III 72 Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'ROLAND FR-1XB Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Pianoforti digitali' => [
-                    'CASIO Casiotone CT S300',
-                    'KORG B2N',
-                    'YAMAHA YDPS54 Black',
+                    'CASIO Casiotone CT S300' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'KORG B2N' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA YDPS54 Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
             ],
             'Batterie' => [
                 'Acustiche' => [
-                    'PEARL Export 16x16 Floor Tom Jet Black',
-                    'DARESTONE CLDRUM White',
-                    'TAMA LJL48S SBO - Club Jam Kit',
+                    'PEARL Export 16x16 Floor Tom Jet Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'DARESTONE CLDRUM White' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'TAMA LJL48S SBO - Club Jam Kit' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Elettroniche' => [
-                    'ALESIS CompactKit 4',
-                    'KORG Stageman 80',
+                    'ALESIS CompactKit 4' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'KORG Stageman 80' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Percussioni' => [
-                    'Triangolo 10 cm. con battente',
-                    'MEINL SH51 Mini Cajon Shaker',
+                    'Triangolo 10 cm. con battente' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'MEINL SH51 Mini Cajon Shaker' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
             ],
             'Fiati' => [
                 'Sax' => [
-                    'YAMAHA Venova YVS120',
-                    'GRASSI BS210',
+                    'YAMAHA Venova YVS120' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'GRASSI BS210' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Trombe' => [
-                    'PTRUMPET Red',
-                    'YAMAHA YTR2330',
+                    'PTRUMPET Red' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA YTR2330' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Flauti' => [
-                    'AULOS G302A',
-                    'GRASSI 720MKII',
-                    'HOHNER 9556',
+                    'AULOS G302A' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'GRASSI 720MKII' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'HOHNER 9556' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Clarinetti' => [
-                    'GRASSI SCL390',
-                    'YAMAHA YCL450E',
+                    'GRASSI SCL390' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA YCL450E' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Altri strumenti a fiato' => [
-                    'Kazoo in Metallo',
-                    'Flauto di Pan 8 Toni DO-DO',
-                    'YAMAHA YBB321 Gold Laquered',
+                    'Kazoo in Metallo' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'Flauto di Pan 8 Toni DO-DO' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA YBB321 Gold Laquered' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
             ],
             'Attrezzature' => [
                 'Amplificatori' => [
-                    'FENDER Champion 40',
-                    'VOX AC2 RhythmVOX Bass',
-                    'BEHRINGER AMP800 MiniAmp',
+                    'FENDER Champion 40' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'VOX AC2 RhythmVOX Bass' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'BEHRINGER AMP800 MiniAmp' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Cavi e connettori' => [
-                    'Female RCA / Male Jack 6.35 Mono Adapter',
-                    'Electraline 10839 Cavo Hi-Fi 03VH-H per Altoparlanti',
-                    'QUIKLOK G108',
+                    'Female RCA / Male Jack 6.35 Mono Adapter' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'Electraline 10839 Cavo Hi-Fi 03VH-H per Altoparlanti' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'QUIKLOK G108' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
                 'Supporti' => [
-                    'Supporto Ripiegabile per 5 Chitarre',
-                    'KONIG & MEYER 15240 Flugelhorn Stand Black',
-                    'YAMAHA Supporto per Stagepas',
+                    'Supporto Ripiegabile per 5 Chitarre' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'KONIG & MEYER 15240 Flugelhorn Stand Black' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
+                    'YAMAHA Supporto per Stagepas' => [
+                        'descrizione_breve' => '',
+                        'descrizione_estesa' => '',
+                        'percorso_foto' => '',
+                    ],
                 ],
             ],
         ];
@@ -133,21 +369,21 @@ class ProdottoSeeder extends Seeder
             foreach ( $sottocategorie as $sottocategoria => $prodotti)
             {
                 $id_sotto_categoria = DB::table('sotto_categoria')
-                    ->join('categoria', 'categoria.id_categoria', '=', 'sotto_categoria.id_categoria')
+                    ->join('categoria','categoria.id_categoria', '=','sotto_categoria.id_categoria')
                     ->where('nome_categoria', $categoria)
                     ->where('nome_sotto_categoria', $sottocategoria)
                     ->select('sotto_categoria.*')
                     ->first()
                     ->id_sotto_categoria;
 
-                foreach ($prodotti as $prodotto) {
+                foreach ($prodotti as $prodotto => $dati_prodotto) {
 
                     DB::table('prodotto')->insert([
                         'nome_prodotto' => $prodotto,
-                        'descrizione_breve' => '',
-                        'descrizione_estesa' => '',
+                        'descrizione_breve' => $dati_prodotto['descrizione_breve'],
+                        'descrizione_estesa' => $dati_prodotto['descrizione_estesa'],
                         'id_sotto_categoria' => $id_sotto_categoria,
-                        'percorso_foto' => '',
+                        'percorso_foto' => $dati_prodotto['percorso_foto'],
                         'prezzo' => rand(200,2000),
                     ]);
                 }
